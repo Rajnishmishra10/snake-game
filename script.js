90024bb7344e08338104d557538eaeb282abcba2
Rajnish Mishra <rm924581@gmail.com>
2026-09-18 09:53:32 +0530

    snake-game

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { isAwaitKeyword } = require("typescript");
+
 document.addEventListener('DOMContentLoaded', function () {
 
     const gameArena = document.getElementById("game-arena");
@@ -7,13 +9,80 @@ document.addEventListener('DOMContentLoaded', function () {
     let gameStarted = false; // Game status
     let food = {x:300, y:200}; //{x: 15*20, y: 10*20} --> cell coordinate ---> pixels
     let snake = [{x:160, y:200}, {x:140, y:200}, {x:120, y:200}];
-
     
+    let dx = cellSize; //+20 
+    let dy = 0
+
+    function updateSnake(){
+        const newHead = {x:snake[0].x + dx, y:snake[0].y + dy };
+        snake.unshift(newHead); // Add new head to new snake
+
+        //check collision with food
+        if(newHead.x == food.x && newHead.y == food.y) {
+            score += 10;
+            // TODO: move food
+            
+
+        }else {
+            snake.pop(); // Remove tail
+        }
+    }
+
+    function changeDirection(e) {
+        console.log("Key passed", e);
+        const isGoingDown = dy == cellSize;
+        const isGoingUP = dy == -cellSize;
+        const isGoingLeft = dy == cellSize;
+        const isGoingRight = dy == -cellSize;
+
+        if(e.kay == 'ArrowUp' && !isGoingDown) {
+            dx = 0;
+            dy = -cellSize;
+        } else if(e.key == 'ArrowDown' && !isGoingUP) {
+            dx = 0;
+            dy = cellSize;
+        } else if(e.key == 'ArrowLeft' && !isGoingRight) {
+            dx = -cellSize;
+            dy = 0;
+        }
+        else if(e.key == 'ArrowRight' && !isGoingLeft) {
+            dx = cellSize;
+            dy = 0;
+        }
+    }
+    function drawDiv(x, y, className) {
+        const divElement = document.createElement('div');;
+        divElement.classList.add(className);
+        divElement.style.top = `${y}px`;
+        divElement.style.left = `${x}px`;
+        return divElement; 
+    }
+
+    function drawFoodAndSnake() {
+        gameArena.innerHTML = ''; // Clear the game arena 
+        // wipe out everything and redraw with new position
+
+        snake.forEach((snakeCell) => {
+            const snakeElement = drawDiv(snakeCell.x, snakeCell.y, 'snake');
+            gameArena.appendChild(snakeElement);
+        });
+
+        const foodElement = drawDiv(food.x, food.y, 'food');
+        gameArena.appendChild(foodElement);
+    }
+
+    function gameLoop() {
+        setInterval(() => {
+            updateSnake();
+            drawFoodAndSnake();
+        }, 200);
+    }
 
     function runGame() {
         if(!gameStarted) {
             gameStarted = true;
-            // gameLoop(); TODO: Implement game loop
+            document.addEventListener('Keyword', changeDirection);
+            gameLoop(); //TODO: Implement game loop
         }
     }
 
